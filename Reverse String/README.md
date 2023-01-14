085294290dcf45e0a1fbf520776df19fc50c7700
AdamTempest <jj030469@gmail.com>
2023-01-14 19:30:27 +0630

    String Operation

---
+++ Reverse String/README.md
@@ -0,0 +1,6 @@
+# Reverse a string
+
+For example: 
+```
+input: "cool" output: "looc"
+```
---
+++ Reverse String/reverse a string.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    	<meta charset="utf-8">
+    	<title>reverse String</title>
+        <style>
+            body {
+                background-color: black;
+                color: white;
+                font-size: 30px;
+            }
+        </style>
+    </head>
+    <body>
+    <?php
+
+    function reverseString($str){
+        $reversed = "";
+        for ($i=strlen($str)-1;$i>=0;$i--){
+            $reversed .= $str[$i];
+        }  
+        return $reversed;
+    }
+
+    $str = " ";
+    echo "Reversed \"". $str ."\": ". reverseString($str) . "<br>";
+    $str = "cool";
+    echo "Reversed \"". $str ."\": ". reverseString($str) . "<br>";
+    $str = "rigid";
+    echo "Reversed \"". $str ."\": ". reverseString($str) . "<br>";
+    $str = "digir";
+    echo "Reversed \"". $str ."\": ". reverseString($str) . "<br>";
+    $str = "looc";
+    echo "Reversed \"". $str ."\": ". reverseString($str) . "<br>";
+
+    ?>
+    </body>
+</html>
